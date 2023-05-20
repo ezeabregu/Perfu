0e229bdcb354eec11d452f9705fd7828fb3a9492
ezeabregu <ezeabregu.ea@gmail.com>
2023-05-20 18:19:02 -0300

    add .

---
+++ info.js
@@ -0,0 +1,50 @@
+const curiosidades=[
+    {
+        id:1,
+        texto:"¿Sabes que la palabra perfume procede del latín per “por” y fumare “a través del humo”? Hacía referencia a la sustancia aromática que desprendía un humo fragante al ser quemada.",
+    },
+    {
+        id:2,
+        texto:"¿Adivinas qué civilización fue la que inventó el perfume? Pues nada más y nada menos que los sumerios, 3500 años antes de Cristo. También desarrollaron ungüentos varios.",
+    },
+    {
+        id:3,
+        texto:"Los egipcios fueron los primeros en perfumarse con regularidad. Hacia el año 1558 antes de Cristo se convirtió en una auténtica obsesión nacional. De hecho Cleopatra era una artista del perfume.",
+    },
+    {
+        id:4,
+        texto:"Perfumarse también es de chicos duros y es que los gladiadores antes de salir a combatir se echaban una loción aromática diferente en varias partes de su cuerpo. En la antigua Roma la obsesión llegó a ser tal, que tanto hombres como mujeres tomaban baños de perfume.",
+    },
+    {
+        id:5,
+        texto:"En el palacio de Luis XV ya hicieron sus pinitos con los ambientadores y es que los criados bañaban a las palomas en distintos perfumes y las liberaban durante las fiestas. Así, esparcían los aromas, allá por donde volasen. Debes saber que en esa época no se lavaban mucho (o nada) y para disimular el mal olor, se perfumaban muchísimo.",
+    },
+    {
+        id:6,
+        texto:"Como sabrás, el PH de la piel mide su acidez. Por lo general, no cambia mucho de una persona a otra pero esta mínima diferencia es suficiente para que uno o varios de los ingredientes que conforman la fragancia, interactúe de manera diferente en cada piel.",
+    },
+    {
+        id:7,
+        texto:"¿Dura lo mismo un perfume en una piel que en otra? Las pieles morenas suelen tender a grasas y es esta grasa que produce el organismo la que, en contacto con los aceites del perfume, hace que el aroma sea más intenso. A estas personas les vienen bien los perfumes intensos, florales o cítricos, a base de civeta, tabaco, almizcle, ámbar, incienso y rosas. Lo contrario ocurre con las pieles blancas, que suelen ser secas. Para estas pieles, lo mejor son los aromas frescos y estimulantes.",
+    },
+    {
+        id:8,
+        texto:"¿Sabes que las españolas somos las que más nos perfumamos de Europa? Nos siguen las italianas y las francesas.",
+    },
+    {
+        id:9,
+        texto:"¿Qué es una eau de cologne? Es una solución compuesta por alrededor de un 3% de perfume en una base de alcohol y agua. Es el aroma más ligero y menos duradero.",
+    },
+    {
+        id:10,
+        texto:"La eau de toilette es una solución compuesta por entre un 3% y un 8% en una base de agua o de alcohol. Menos concentrada que la eau de parfum, suele ser utilizada para crear una versión más suave de una fragancia.",
+    },
+    {
+        id:11,
+        texto:"Una eau de parfum es una solución de perfume en alcohol conteniendo entre un 10% y 15% de perfume.",
+    },
+    {
+        id:12,
+        texto:"La fragancia que contiene entre un 20% y 50% de perfume sería calificada como perfume. Es la opción más duradera e intensa, y la más cara.",
+    },
+];
